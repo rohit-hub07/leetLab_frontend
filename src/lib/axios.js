bb462b8b98d28b5e88534e42a02c75a7a4eaaf95
rohitsingh <rohitsingh986491@gmail.com>
2025-06-06 21:34:35 +0530

    changed the backedn url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:import.meta.env.MODE === "development"? "http://localhost:8080/api/v1" : "https://leetlab-backend-ifrk.onrender.com/",
+  baseURL:import.meta.env.MODE === "development"? "http://localhost:8080/api/v1" : "https://leetlab-backend-ifrk.onrender.com/api/v1",
   withCredentials: true,
 });
 
